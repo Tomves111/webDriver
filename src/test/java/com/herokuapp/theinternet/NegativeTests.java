14a6effc613c565d1646ecdd8c1bfba9f5357006
Tomáš Veselý <tvesely@foxconndrc.com>
2019-02-05 12:05:23 +0100

    "negativeTests"

--- src/test/java/com/herokuapp/theinternet/NegativeTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -12,41 +12,43 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.AfterMethod;
 
-
 public class NegativeTests {
-		WebDriver driver;
-		
-		@Parameters({ "browser" })
-	    @BeforeMethod
-		protected void setUp(@Optional("chrome")String browser ) {
-	    	// Create driver
-	    	System.out.println("Create driver:" + browser);
-	    	
-	    	switch (browser) {
-			case "chrome":
-				System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-			    driver = new ChromeDriver();
-				break;
-			
-			case "firefox":
-				System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodrive");
-				driver = new FirefoxDriver();
-			default:
-				break;
-			}
-	    	
-	
+	WebDriver driver;
 
-			sleep(3000);
+	@Parameters({ "browser" })
+	@BeforeMethod
+	protected void setUp(@Optional("chrome") String browser) {
+		// Create driver
+		System.out.println("Create driver:" + browser);
 
-			driver.manage().window().maximize();
-			
+		switch (browser) {
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
+
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver.exe");
+			driver = new FirefoxDriver();
+
+		default:
+			System.out.println("Do not know how to start:" + browser + "starting chrome.");
+			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+			driver = new ChromeDriver();
+			break;
 		}
-    @Parameters({ "username", "password", "expectedMessage" })
-	@Test(priority = 1, groups = { "smokeTests", "negativeTests"})
+
+		sleep(3000);
+
+		driver.manage().window().maximize();
+
+	}
+
+	@Parameters({ "username", "password", "expectedMessage" })
+	@Test(priority = 1, groups = { "smokeTests", "negativeTests" })
 	public void incorrectUsernameTest() {
 		System.out.println("Starting incorrectUsernameTest");
-		
+
 		// driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
 		// open the page
@@ -63,7 +65,6 @@ public class NegativeTests {
 		WebElement password = driver.findElement(By.id("password"));
 		password.sendKeys("SuperSecretPassword!");
 
-
 		// push log in button
 		WebElement logInButton = driver.findElement(By.className("radius"));
 		logInButton.click();
@@ -75,20 +76,17 @@ public class NegativeTests {
 		Assert.assertTrue(actualErrorMessage.contains(expectedErrorMessage),
 				"actualErrorMessage does not contain expectedErrorMessage\nexpectedSuccessMessage: "
 						+ expectedErrorMessage + "\nactualErrorMessage: " + actualErrorMessage);
-		
-
 
 	}
-    
-    @AfterMethod
-	protected void tearDown( ) {
-    	sleep(3000);
-    	System.out.println("Close Driver");
+
+	@AfterMethod
+	protected void tearDown() {
+		sleep(3000);
+		System.out.println("Close Driver");
 		// Close browser
 		driver.quit();
-		
+
 	}
-    
 
 	/** Static sleep */
 	private void sleep(long millis) {
